8924af31f5873b1215d14a063e887a67a88fbd13
franco.fral <franck_ral@hotmail.com>
2020-07-27 13:18:26 -0400

    first-react-app: Understanding & Using State

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -5,20 +5,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  // ? This only works in class based components
+  state = {
+    persons: [
+      { name: 'Franco', age: 30 },
+      { name: 'Fernando', age: 27 },
+      { name: 'Mireya', age: 20 }
+    ]
+  }
+
   render() {
     return (
       // ! Important: We can only have one root element in JSX code.
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Franco" age="30" />
-        <Person name="Fernando" age="27" >My Hobbies: Reading</Person>
-        <Person name="Mireya" age="20" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
 
   // render() {
-    // ? React will interprete by default h1 as a string    
+    // ? React will interprete by default h1 as a string
     // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App');
     // ? So If we would need to create another html/react element, we would need to do the following
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
