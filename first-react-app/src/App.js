279f250883711844c6a6d204af8debabe7e91b46
franco.fral <franck_ral@hotmail.com>
2020-07-27 12:31:51 -0400

    first-react-app: Understanding JSX

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <h1>Hi, I'm a React App</h1>
+  //     </div>
+  //   );
+  // }
+
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+    // ? React will interprete by default h1 as a string    
+    // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App');
+    // ? So If we would need to create another html/react element, we would need to do the following
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
