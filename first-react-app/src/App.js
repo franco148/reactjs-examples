62beb0ed31f745b6a183b07e94df5c67de6519b0
franco.fral <franck_ral@hotmail.com>
2020-07-27 12:42:08 -0400

    first-react-app: Creating a Functional Component

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  // render() {
-  //   return (
-  //     <div className="App">
-  //       <h1>Hi, I'm a React App</h1>
-  //     </div>
-  //   );
-  // }
-
   render() {
+    return (
+      // ! Important: We can only have one root element in JSX code.
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <Person />
+      </div>
+    );
+  }
+
+  // render() {
     // ? React will interprete by default h1 as a string    
     // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App');
     // ? So If we would need to create another html/react element, we would need to do the following
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
-  }
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+  // }
 }
 
 export default App;
---
+++ first-react-app/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// ES6 Approach - Functional Component
+const person = () => {
+    return <p>I'm a Person!</p>;
+};
+
+export default person;
