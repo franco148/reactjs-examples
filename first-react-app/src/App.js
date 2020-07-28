3a2ebd9cceff5fe4bf82d0d1a8fa811e5f0f7daa
franco.fral <franck_ral@hotmail.com>
2020-07-27 22:51:59 -0400

    reactjs-list-conditions: Section 4 - Flexible Lists

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
   // ? This only works in class based components
   state = {
     persons: [
-      { name: 'Franco', age: 30 },
-      { name: 'Fernando', age: 27 },
-      { name: 'Mireya', age: 20 }
+      { id: 'pid1', name: 'Franco', age: 30 },
+      { id: 'pid2', name: 'Fernando', age: 27 },
+      { id: 'pid3', name: 'Mireya', age: 20 }
     ]
   }
 
@@ -39,15 +39,23 @@ class App extends Component {
     this.setState({ persons: persons});
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'TwoWay Binding', age: 30 },
-        { name: event.target.value, age: 27 },
-        { name: 'Mireya', age: 18 }
-      ],
-      showPersons: false
+  nameChangeHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {...this.state.persons[personIndex]};
+
+    // Ahother approach to the previous one would be.
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -69,12 +77,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(({name, age}, index) => {
+          {this.state.persons.map(({id, name, age}, index) => {
             return <Person
-                    key={index}
+                    key={id}
                     name={name}
                     age={age}
-                    click={() => this.deletePersonHandler(index)} />;
+                    click={() => this.deletePersonHandler(index)}
+                    changed={(event) => this.nameChangeHandler(event, id) } />;
           })}
           {/* <Person
             name={this.state.persons[0].name}
