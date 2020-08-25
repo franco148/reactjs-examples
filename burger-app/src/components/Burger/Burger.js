7ef68e9bd3a39295162090c7bc32faf73b94bd7d
franco.fral <franck_ral@hotmail.com>
2020-08-24 23:16:15 -0400

    reactjs-max-burger-app: Section 8 - Adding the Build Control Component

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -5,13 +5,20 @@ import BurgerIngredient from './Ingredient/Ingredient';
 import styles from './Burger.module.css';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
       .map(ingKey => {
         return [...Array(props.ingredients[ingKey])]
               .map((_, i) => {
                 return <BurgerIngredient key={ingKey + i} type={ingKey} />;
-              });
+              })
+              .reduce((arr, el) => {
+                return arr.concat(el);
+              }, []);
       });
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     // <div className={styles.Burger}>
