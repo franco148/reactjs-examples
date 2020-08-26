301839291eb31cb283d192a2cdbbd25af3b1c223
franco.fral <franck_ral@hotmail.com>
2020-08-25 22:16:46 -0400

    reactjs-max-burger-app: Section 8 - Adding a Custom Button Component

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,6 +87,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: true });
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  }
+
   render() {
     // {salad: true, meat: false, ...}
     const disabledInfo = {
@@ -99,7 +103,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
