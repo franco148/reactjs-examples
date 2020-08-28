04dc9d1e336810c441af5f86cba95cc72c63f963
franco.fral <franck_ral@hotmail.com>
2020-08-28 11:09:52 -0400

    reactjs-max-burger-app: Section 8 - Improving performance

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+// We are converting this to a Class Based component in order to
+// see how this is being rendered (to fix performance issues)
+class OrderSummary extends Component {
+
+  // ? This could be a functional component, does not have to be a class.
+  componentDidUpdate() {
+    // Here we can verify that ORDER SUMMARY is being updated, even if it
+    // is not being displayed. That would cause a performance issue.
+    console.log('[OrderSummary] will update');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(ingKey => {
       return (
         <li key={ingKey}>
           <span style={{textTransform: 'capitalize'}}>{ingKey}</span>:
-          {props.ingredients[ingKey]}
+          {this.props.ingredients[ingKey]}
         </li>
       );
     });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
--- burger-app/src/components/UI/Modal/Modal.js
+++ burger-app/src/components/UI/Modal/Modal.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Modal.module.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={styles.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+// We are converting this to a Class Based component in order to
+// see how this is being rendered (to fix performance issues)
+class Modal extends Component {
 
-export default modal;
+  shouldComponentUpdate(nextProps, nextState) {
+    // With this we are avoiding to re-render or update the ORDER SUMMARY
+    // unnecesary.
+    return nextProps.show !== this.props.show;
+  }
+
+  componentDidUpdate() {
+    console.log('[Modal] will update');
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
