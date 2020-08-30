1bf3202ba2f3fa4fd2c00266bf0b56cf696f483b
franco.fral <franck_ral@hotmail.com>
2020-08-30 12:33:40 -0400

    reactjs-max-http-requests: Section 9 - POSTing Data to the Server & Sending a DELETE Request

--- http-react-app/src/components/NewPost/NewPost.js
+++ http-react-app/src/components/NewPost/NewPost.js
@@ -1,31 +1,44 @@
 import React, { Component } from 'react';
+import http from 'axios';
 
 import './NewPost.css';
 
 class NewPost extends Component {
-    state = {
-        title: '',
-        content: '',
-        author: 'Max'
-    }
+  state = {
+    title: '',
+    content: '',
+    author: 'Franco'
+  }
 
-    render () {
-        return (
-            <div className="NewPost">
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
-                </select>
-                <button>Add Post</button>
-            </div>
-        );
-    }
+  postDataHandler = () => {
+    const post = {
+      title: this.state.title,
+      body: this.state.content,
+      author: this.state.author
+    };
+    http.post('http://jsonplaceholder.typicode.com/posts', post)
+      .then(response => {
+        console.log(response);
+      });
+  }
+
+  render () {
+    return (
+      <div className="NewPost">
+        <h1>Add a Post</h1>
+        <label>Title</label>
+        <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+        <label>Content</label>
+        <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+        <label>Author</label>
+        <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+          <option value="Max">Franco</option>
+          <option value="Manu">Fercho</option>
+        </select>
+        <button onClick={this.postDataHandler}>Add Post</button>
+      </div>
+    );
+  }
 }
 
 export default NewPost;
