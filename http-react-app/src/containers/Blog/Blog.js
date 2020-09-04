d8b870a298449f7d6028aa7ddb2285bfdd7df5d7
franco.fral <franck_ral@hotmail.com>
2020-09-04 17:12:38 -0400

    reactjs-max-routing-spa: Section 11 - Preparing the Project for Routing

--- http-react-app/src/containers/Blog/Blog.js
+++ http-react-app/src/containers/Blog/Blog.js
@@ -1,48 +1,11 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
 
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
 
-  state = {
-    posts: [],
-    selectedPostId: undefined
-  }
-
-  componentDidMount() {
-    axios.get('/posts')
-          .then(response => {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-              return {
-                ...post,
-                author: 'Franco'
-              }
-            });
-            console.log(updatedPosts);
-            this.setState({posts: updatedPosts});
-          })
-          .catch(error => {
-            console.log('AN ERROR OCURRED! ', error);
-          });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  }
-
   render () {
-
-    const posts = this.state.posts.map(post => {
-      return <Post 
-        key={post.id} 
-        title={post.title} 
-        author={post.author}
-        clicked={() => this.postSelectedHandler(post.id)} />;
-    });
-
     return (
       <div className="Blog">
         <header>
@@ -53,9 +16,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">
-          {posts}
-        </section>
+        <Posts />
       </div>
     );
   }
