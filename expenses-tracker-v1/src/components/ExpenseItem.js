196d583351ed55584c373cd5516a67dd0b65ee73
Franco Arratia <franck_ral@hotmail.com>
2023-01-20 16:01:48 -0400

    Section3-WorkingWithComponents: Passing data via props

--- expenses-tracker-v1/src/components/ExpenseItem.js
+++ expenses-tracker-v1/src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
     const expenseDate = new Date(2023, 1, 20);
     const expenseTitle = 'Car Insurance';
@@ -8,10 +8,10 @@ function ExpenseItem() {
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toDateString()}</div>
+            <div>{props.date.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
