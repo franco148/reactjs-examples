a4facd1c5aa3f4f28692cacb17ac139ec3b97dea
Franco Arratia <franck_ral@hotmail.com>
2023-01-20 11:25:57 -0400

    Section3-WorkingWithComponents: Building a first custom component

--- expenses-tracker-v1/src/App.js
+++ expenses-tracker-v1/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <ExpenseItem></ExpenseItem>
+      </header>      
     </div>
   );
 }
