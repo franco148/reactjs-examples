c50e6485d243b54c00182d41e6fc4cafea608d66
Franco Arratia <franck_ral@hotmail.com>
2023-01-24 15:13:21 -0400

    Section4-ReactState-WorkingWithEvents: Updating the state that depends on the previous state

--- expenses-tracker-v1/src/components/NewExpense/ExpenseForm.js
+++ expenses-tracker-v1/src/components/NewExpense/ExpenseForm.js
@@ -16,25 +16,40 @@ const ExpenseForm = () => {
   const titleChangeHandler = (event) => {
     // console.log("Title changed!" + event.target.value);
     // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
     });
   };
 
   const amountChangeHandler = (event) => {
     // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
     });
   };
 
   const dateChangeHandler = (event) => {
     // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
     });
   };
 
