505006c258ce02cbb25a8004ad958f5214515b22
Franco Arratia <franck_ral@hotmail.com>
2023-01-25 21:02:58 -0400

    Section5-RenderingConditionalContent: rendering conditional content on ExpenseForm

--- expenses-tracker-v1/src/components/NewExpense/NewExpense.js
+++ expenses-tracker-v1/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,11 +15,27 @@ const NewExpense = (props) => {
     // console.log(expenseData);
 
     props.onAddExpense(expenseData);
+    setIsEditing(() => false);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(() => false);
+  };
+  const startEditingHandler = () => {
+    setIsEditing(() => true);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
