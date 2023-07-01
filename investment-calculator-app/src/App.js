c160e404ecdb2ef9d1522fdc2c6cc3e0771fbd2e
Franco Arratia <franck_ral@hotmail.com>
2023-06-30 22:42:23 -0400

    investment-calculator-app: Integrated the changes

--- investment-calculator-app/src/App.js
+++ investment-calculator-app/src/App.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import ResultsTable from "./components/ResultsTable/ResultsTable";
 import UserInput from "./components/UserInput/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState(null);
+
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+    setUserInput(userInput);
+  };
 
-    const yearlyData = []; // per-year results
+  // Should be triggered when form is submitted
+  // You might not directly want to bind it to the submit event on the form though...
 
+  const yearlyData = []; // per-year results
+
+  if (userInput) {
     let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput["expected-return"] / 100;
@@ -28,18 +35,26 @@ function App() {
     }
 
     // do something with yearlyData ...
-  };
+  }
 
   return (
     <div>
       <Header />
 
-      <UserInput />
+      <UserInput onCalculate={calculateHandler} />
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <ResultsTable />
+      {!userInput && (
+        <p style={{ textAlign: "center" }}>No investment calculated yet.</p>
+      )}
+      {userInput && (
+        <ResultsTable
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
     </div>
   );
 }
--- investment-calculator-app/src/components/Header/Header.js
+++ investment-calculator-app/src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import logo from "../../assets/investment-calculator-logo.png";
+import clasess from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={clasess.header}>
       <img src={logo} alt="logo" />
       <h1>Investment Calculator</h1>
     </header>
